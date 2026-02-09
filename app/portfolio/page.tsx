633e7c6ce77e0f3004dd0f17a7782f2c8ab346a2
yasukamiytb-ai <yasukami.ytb@gmail.com>
2026-02-09 19:20:10 +0100

    Create main portfolio page importing PortfolioGallery component

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import PortfolioGallery from '../components/PortfolioGallery';
+
+const PortfolioPage = () => {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <PortfolioGallery />
+        </div>
+    );
+};
+
+export default PortfolioPage;
